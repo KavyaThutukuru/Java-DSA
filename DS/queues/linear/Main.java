b10d18cbb6f262c9e165699e5cf84c0750c5f860
KavyaThutukuru <kavyathutukuru@gmail.com>
2025-08-05 20:49:53 +0530

    Add files via upload

---
+++ DS/queues/linear/Main.java
@@ -0,0 +1,66 @@
+package queue.linear;
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter Size: ");
+        int size=sc.nextInt();
+        Operations op=new Operations(size);
+        int ch;
+        do{
+            System.out.println("\n1. Enqueue the element");
+            System.out.println("2. Dequeue the element");
+            System.out.println("3. check queue is empty");
+            System.out.println("4. check queue is full");
+            System.out.println("5. Get front element");
+            System.out.println("6. Get rear element");
+            System.out.println("7. display queue");
+            System.out.println("8. Exit");
+            System.out.print("Enter choice:");
+            ch=sc.nextInt();
+            switch(ch){
+                case 1:
+                    System.out.print("Enter an element to insert into the queue:");
+                    int ele=sc.nextInt();
+                    op.enqueue(ele);
+                    break;
+                case 2:
+                    op.dequeue();
+                    break;
+                case 3:
+                    if(op.isEmpty()){
+                        System.out.println("Queue is empty");
+                    }else{
+                        System.out.println("Queue is not empty");
+                    }
+                    break;
+                case 4:
+                    if(op.isFull()){
+                        System.out.println("Queue is full");
+                    }else{
+                        System.out.println("Queue is not full");
+                    }
+                    break;
+                case 5:
+                    Integer frontEle=op.getFront();
+                    if(frontEle!=null)
+                        System.out.println("Front: "+frontEle);
+                    break;
+                case 6:
+                    Integer rearEle=op.getRear();
+                    if(rearEle!=null)
+                        System.out.println("Rear: "+rearEle);
+                    break;
+                case 7:
+                    op.display();
+                    break;
+                case 8:
+                    System.out.println("Exiting...");
+                    break;
+                default:
+                    System.out.println("Invalid choice");
+            }
+        }while(ch!=8);
+        sc.close();
+    }
+}
---
+++ DS/queues/linear/Operations.java
@@ -0,0 +1,64 @@
+package queue.linear;
+public class Operations {
+    private int maxSize;
+    private int[] queue;
+    private int rear;
+    private int front;
+    Operations(int size){
+        this.rear=-1;
+        this.front=-1;
+        this.queue=new int[size];
+        maxSize=size;
+    }
+    public void enqueue(int ele){
+        if(isFull()){
+            System.out.println("Queue is Full");
+            return;
+        }
+        if (front==-1) front = 0; 
+        queue[++rear]=ele;
+    }
+    public void dequeue(){
+        if(isEmpty()){
+            System.out.println("Queue is empty");
+            return;
+        }
+        System.out.println("Deleted element is "+queue[front]);
+        front++;
+        if (front > rear){ 
+            front = -1;
+            rear = -1;
+        }
+    }
+    public boolean isFull(){
+        return rear==maxSize-1;
+    }
+    public boolean isEmpty(){
+        return front == -1;
+    }
+    public void display(){
+        if(isEmpty()){
+            System.out.println("Queue is empty");
+            return;
+        }
+        for(int i=front;i<=rear;i++){
+            System.out.print(queue[i]+" ");
+        }
+        System.out.println();
+    }
+    public Integer getFront() {
+        if(isEmpty()) {
+            System.out.println("Queue is empty");
+            return null;
+        }
+        return queue[front];
+    }
+    
+    public Integer getRear() {
+        if(isEmpty()){
+            System.out.println("Queue is empty");
+            return null;
+        }
+        return queue[rear];
+    }
+}
